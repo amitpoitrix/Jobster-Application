049581dc994d3d8d0753555054b33d8407b5e2e3
amitpoitrix <amit.poitrix@gmail.com>
2022-11-17 21:43:23 +0530

    Some changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -43,7 +43,7 @@ const login = async (req, res) => {
         throw new UnauthenticatedError('Invalid Password');
     }
 
-    // Getting the token for the user who provided its credentials
+    // Getting the token for the user who provided its credential
     const token = user.createJWT();
     res.status(StatusCodes.OK).json({
         user: {
